c88fda1a0bf0315fe473802e061bd507b06dbc89
theringmaster <gzroms@hotmail.com>
2016-01-18 19:09:02 -0500

    Add tm and aapt

--- configs/themes_common.mk
+++ configs/themes_common.mk
@@ -1,7 +1,9 @@
 # Theme engine
 PRODUCT_PACKAGES += \
+    aapt \
     ThemeChooser \
-    ThemesProvider
+    ThemesProvider \
+    tm
 
 PRODUCT_COPY_FILES += \
     vendor/validus/prebuilt/etc/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml \
