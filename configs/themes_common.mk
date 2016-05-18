voidz777-pure/android_vendor_pure

Add CMTE permissions/feature

configs/themes_common.mk
@@ -1,0 +1,8 @@
+# Theme engine
+PRODUCT_PACKAGES += \
+    ThemeChooser \
+    ThemesProvider
+
+PRODUCT_COPY_FILES += \
+    vendor/nexus/prebuilt/etc/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml \
+    vendor/nexus/prebuilt/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml

rc420head-M/vendor_candy

configs/themes_common.mk
@@ -1,0 +1,8 @@
+# Theme engine
+PRODUCT_PACKAGES += \
+    ThemeChooser \
+    ThemesProvider
+
+PRODUCT_COPY_FILES += \
+    vendor/validus/prebuilt/etc/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml \
+    vendor/validus/prebuilt/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml